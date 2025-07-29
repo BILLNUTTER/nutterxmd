01a49e1c1485179cbcdefccc36af3d03f1ca9a62
omayio <samwriters006@gmail.com>
2025-07-29 23:00:27 +0300

    fix: cleaned dist and rebuilt successfully

--- shared/globals.d.ts
+++ shared/globals.d.ts
@@ -1,4 +1,10 @@
+import "express-serve-static-core";
 declare module 'qrcode';
 declare module 'qrcode-terminal';
 declare module 'gtts';
 declare module 'some-lib-name';
+declare module "express-serve-static-core" {
+    interface Request {
+        userId?: string;
+    }
+}
